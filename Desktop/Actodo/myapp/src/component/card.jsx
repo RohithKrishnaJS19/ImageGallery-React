bf1b93f0f6a21a38e5565fa78a791a6aba737b2f
Rohith Krishna <rohithkrishnajs1908@gmail.com>
2026-04-09 13:54:27 +0530

    Delete Desktop/Actodo/myapp directory

--- Desktop/Actodo/myapp/src/component/card.jsx
+++
@@ -1,9 +0,0 @@
-function Card(props) {
-    return (
-        <div style={{ backgroundColor: props.bg }} className="px-10 py-5  border rounded-md text-center flex-grow">
-            <h1 className="text-2xl font-medium">{props.title}</h1>
-            <p>{props.sub}</p>
-        </div>
-    )
-}
-export default Card;
